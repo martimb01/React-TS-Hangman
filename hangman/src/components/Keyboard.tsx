505a120afc30b90e9440819a966434241884b54e
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-06-03 16:47:13 +0100

    Initial Keyboard component implementation done

--- hangman/src/components/Keyboard.tsx
+++ hangman/src/components/Keyboard.tsx
@@ -1,5 +1,44 @@
-function Keyboard() {
+import style from "../css/Keyboard.module.css"
+const KEYS: string[] = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z"
+]
 
+
+function Keyboard() {
+    return (
+        <div className={style.keyboard}>
+            {KEYS.map((key) => {
+                return (
+                    <button className={style.button} key={key}>{key}</button>
+                )
+            })}
+        </div>
+    )
 }
 
 export default Keyboard;
