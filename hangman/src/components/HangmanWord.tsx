fd78759febea6a4858136f3c8d20d3231ea01e9d
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-06-03 16:19:49 +0100

    Initial HangmanWord component done, moving to the Keyboard component

--- hangman/src/components/HangmanWord.tsx
+++ hangman/src/components/HangmanWord.tsx
@@ -1,5 +1,21 @@
-function HangmanWord() {
+import style from "../css/HangmanWord.module.css"
+const word = "testerino"
+const guessedLetters = "e"
 
+function HangmanWord() {
+    return (
+        <div className={style.mainContainer}>
+            {word.split("").map((letter, index) => {
+                return <span className={style.letter}>
+                            <span style={{
+                                visibility: guessedLetters.includes(letter) ? "visible" : "hidden"
+                            }} key={index}>
+                                {letter}
+                            </span>  
+                        </span>
+            })}
+        </div>
+    )
 }
 
 export default HangmanWord;
